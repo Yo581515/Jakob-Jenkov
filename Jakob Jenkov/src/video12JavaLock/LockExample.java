fea75eebdc1c7d212a281d0df76d06944865d028
581515 <47955690+Y581515@users.noreply.github.com>
2021-09-08 07:04:09 +0200

    u tube

--- Jakob Jenkov/src/video12JavaLock/LockExample.java
+++ Jakob Jenkov/src/video12JavaLock/LockExample.java
@@ -6,6 +6,8 @@ import java.util.concurrent.locks.ReentrantLock;
 public class LockExample {
 
 	public static void main(String[] args) throws InterruptedException {
+		
+//		https://www.youtube.com/watch?v=mTGdtC9f4EU&list=PLL8woMHwr36EDxjUoCzboZjedsnhLP1j4
 
 //		Lock lock = new ReentrantLock(true);
 //		lock.lock();
@@ -13,7 +15,7 @@ public class LockExample {
 //		lock.unlock();
 
 //		1)
-//		lockBasics();
+		lockBasics();
 
 //		Thread.sleep(4000);
 //		System.out.println();
@@ -24,42 +26,42 @@ public class LockExample {
 //		Thread.sleep(4000);
 //		System.out.println();
 //      3)
-		tryLock();
+//		tryLock();
 
 	}
 
 //	1)
-//	private static void lockBasics() {
-//		Lock lock = new ReentrantLock(true);
-//		Runnable runnable = () -> {
-//			lockSleepUnlock(lock, 1000);
-//		};
+	private static void lockBasics() {
+		Lock lock = new ReentrantLock(true);
+		Runnable runnable = () -> {
+			lockSleepUnlock(lock, 1000);
+		};
+
+		Thread thread1 = new Thread(runnable, "Thread 1");
+		Thread thread2 = new Thread(runnable, "Thread 2");
+		Thread thread3 = new Thread(runnable, "Thread 3");
+		thread1.start();
+		thread2.start();
+		thread3.start();
+	}
 //
-//		Thread thread1 = new Thread(runnable, "Thread 1");
-//		Thread thread2 = new Thread(runnable, "Thread 2");
-//		Thread thread3 = new Thread(runnable, "Thread 3");
-//		thread1.start();
-//		thread2.start();
-//		thread3.start();
-//	}
+	private static void lockSleepUnlock(Lock lock, int timeMillis) {
+		try {
+			lock.lock();
+			System.out.println(Thread.currentThread().getName() + " holds the lock.");
+			sleep(timeMillis);
+		} finally {
+			lock.unlock();
+		}
+	}
 //
-//	private static void lockSleepUnlock(Lock lock, int timeMillis) {
-//		try {
-//			lock.lock();
-//			System.out.println(Thread.currentThread().getName() + " holds the lock.");
-//			sleep(timeMillis);
-//		} finally {
-//			lock.unlock();
-//		}
-//	}
-//
-//	private static void sleep(int timeMillis) {
-//		try {
-//			Thread.sleep(timeMillis);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	private static void sleep(int timeMillis) {
+		try {
+			Thread.sleep(timeMillis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 //	2)
 //	private static void lockInterrupted() {
